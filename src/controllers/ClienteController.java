73d51aaa7b8c7243dd3a86699e8c42a43e7fb042
PLATYPUSCO007 <37806148+PLATYPUSCO007@users.noreply.github.com>
2018-06-05 17:41:09 -0300

    Funcion Buscar nombre

--- src/controllers/ClienteController.java
+++ src/controllers/ClienteController.java
@@ -18,31 +18,30 @@ import repositorio.SessionFactoryProvider;
  * @author bangho
  */
 public class ClienteController {
-    
-    ClienteDao clienteDao; 
+
+    ClienteDao clienteDao;
     Session session;
     Transaction tx;
     Cliente clienteNuevo = new Cliente();
-    
-    public void guardarClienteNuevo(){
-       try{ 
-          // clienteNuevo.validarDatos();
-           guardarCliente();
-       }
-       catch(Exception e){
-           throw  (new ClienteIncomploteException(e.getMessage()));
-       }
+
+    public void guardarClienteNuevo() {
+        try {
+            // clienteNuevo.validarDatos();
+            guardarCliente();
+        } catch (Exception e) {
+            throw (new ClienteIncomploteException(e.getMessage()));
+        }
     }
-    public void guardarCliente(){
-        
-	session = SessionFactoryProvider.getInstance().createSession();
-	tx = session.beginTransaction();	
-	Runner.addSession(session);
+
+    public void guardarCliente() {
+
+        session = SessionFactoryProvider.getInstance().createSession();
+        tx = session.beginTransaction();
+        Runner.addSession(session);
         clienteDao = new ClienteDao();
         clienteDao.guardar(clienteNuevo);
         tx.commit();
         session.close();
-        
     }
 
     public Cliente getClienteNuevo() {
@@ -52,5 +51,22 @@ public class ClienteController {
     public void setClienteNuevo(Cliente clienteNuevo) {
         this.clienteNuevo = clienteNuevo;
     }
-    
+
+    public String buscarCliente(String campo, String ses) {
+        String resultado = null;
+        session = SessionFactoryProvider.getInstance().createSession();
+        tx = session.beginTransaction();
+        Runner.addSession(session);
+        clienteDao = new ClienteDao();
+        clienteNuevo = clienteDao.recuperar(campo, ses);
+        tx.commit();
+        session.close();
+
+        return " Nombre de cliente: " + clienteNuevo.nombre + "|"
+                + " Direccion: " + clienteNuevo.direccion + "|"
+                + " Telefono: " + clienteNuevo.telefono + "|"
+                + " ID: " + clienteNuevo.id + "|"
+                + " DNI: " + clienteNuevo.DNI;
+    }
+
 }
